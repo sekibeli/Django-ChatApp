685a57abc61e761bb1adeb6ac2cb2a56a7e76532
Julia Georgiew <java2011@me.com>
2023-09-24 21:27:27 +0200

    finish chat model

--- chat/admin.py
+++ chat/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 
 class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','author','text','receiver','created_at')
     list_display = ('created_at','author', 'text','receiver')
-    fields = ('author','text','receiver','created_at')
     search_fields = ('text',)
+   
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
