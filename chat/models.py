9c0b6d196fb89ca6a7be9443f889f57074140abe
Julia Georgiew <java2011@me.com>
2023-09-24 13:46:44 +0200

    add material lite

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
+from django.db.models.fields import DateField
 
 # Create your models here.
+
+class Message(models.Model):
+    text = models.charField(max_length=500)
+    created_at = DateField(default=date.today)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
