56ee4bd3fde26ff32fa3727915bc7295afa0c36c
Julia Georgiew <java2011@me.com>
2023-09-25 19:49:06 +0200

    authentication

--- chat/views.py
+++ chat/views.py
@@ -3,7 +3,7 @@ from .models import Chat, Message
  
 def index(request):
     if request.method == 'POST':
-        print("Received data " + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
         Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
-    return render(request, 'chat/index.html', {'username': 'irgendein Inhalt'})
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {'messages':  chatMessages})
