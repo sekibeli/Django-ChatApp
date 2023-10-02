59a061328074453b625557d7bbc314265e2073cc
Julia Georgiew <java2011@me.com>
2023-10-02 21:44:21 +0200

    chat css

--- chat/models.py
+++ chat/models.py
@@ -11,6 +11,7 @@ class Chat(models.Model):
 class Message(models.Model):
     text = models.CharField(max_length=500)
     created_at = models.DateField(default=date.today)
+  
     chat = models.ForeignKey(Chat, on_delete=models.CASCADE,  related_name='chat_message_set', default=None , blank=True, null=True )
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
